22dd50a7327a196f0d7495265eb657fd423736e6
Mohibur Rahman <mohiburrahman@Mohiburs-MacBook-Air.local>
2026-05-21 09:54:10 +0600

    lep year check

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// even odd
+// //even odd
+// const temp = 20;
+// for (let i = 0; i <= temp; i++) {
+//   if (i == 0) {
+//     continue;
+//   } else if (i % 2 == 0) {
+//     console.log(i, "Even number");
+//   } else {
+//     console.log(i, "Odd Number");
+//   }
+// }
 
-const temp = 20;
+// //Leap year check
 
-for (let i = 0; i <= temp; i++) {
-  if (i == 0) {
-    continue;
-  } else if (i % 2 == 0) {
-    console.log(i, "Even number");
+const isLeapYear = (year) => {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log(year, "Is leap year");
   } else {
-    console.log(i, "Odd Number");
+    console.log(year, "Is not leap year");
   }
-}
+};
+
+isLeapYear(1900);
