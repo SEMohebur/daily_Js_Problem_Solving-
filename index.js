21b315a90a8a1464e3c1ef3c990afebacd15f37b
SEMohebur <mdmohebur11@gmail.com>
2026-05-24 07:31:27 +0600

    reverse string

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 //   if (i == 0) {
 //     continue;
 //   } else if (i % 2 == 0) {
-//     console.log(i, "Even number");
+//     // console.log(i, "Even number");
 //   } else {
-//     console.log(i, "Odd Number");
+//     // console.log(i, "Odd Number");
 //   }
 // }
 
@@ -14,15 +14,15 @@
 
 // const isLeapYear = (year) => {
 //   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-//     console.log(year, "Is leap year");
+//     // console.log(year, "Is leap year");
 //   } else {
-//     console.log(year, "Is not leap year");
+//     // console.log(year, "Is not leap year");
 //   }
 // };
 
 // isLeapYear(1900);
 
-// Find largest between two numbers
+// //Find largest between two numbers/////////////////////////////////////////////////////
 // const findLargestNumber = (params) => {
 //   let temp = params[0];
 //   for (let i = 0; i < params.length; i++) {
@@ -33,8 +33,9 @@
 //   return temp;
 // };
 
-// console.log(findLargestNumber([4, 2, 5, 2, 4, 8, 1, 9, 23, 64, 1]));
+// // console.log(findLargestNumber([4, 2, 5, 2, 4, 8, 1, 9, 23, 64, 1]));
 
+// ///////////////////////////////////////////////////
 // const checkNumber = (number) => {
 //   if (number > 0) {
 //     return "Positive Number";
@@ -45,9 +46,9 @@
 //   }
 // };
 
-// console.log(checkNumber(10));
+// // console.log(checkNumber(10));
 
-//find a longest string
+// //find a longest string  /////////////////////////////////////////////////////
 // const findLongestStr = (arr) => {
 //   let temp = "";
 
@@ -61,17 +62,17 @@
 //   return temp;
 // };
 
-// console.log(
-//   findLongestStr([
-//     "          sdsdsds      ",
-//     "mahir",
-//     "   mmmmm ",
-//     "caw",
-//     " mudassir",
-//   ]),
-// );
+// // console.log(
+// //   findLongestStr([
+// //     "          sdsdsds      ",
+// //     "mahir",
+// //     "   mmmmm ",
+// //     "caw",
+// //     " mudassir",
+// //   ]),
+// // );
 
-// Check voting eligibility (18+)
+// //Check voting eligibility (18+)/////////////////////////////////////////////////////
 // const votingCondition = (age) => {
 //   if (age < 18) {
 //     return "You are not eligible to vote.";
@@ -80,26 +81,41 @@
 //   }
 // };
 
-// console.log(votingCondition(24));
+// // console.log(votingCondition(24));
 
-//Check if character is vowel or consonant
+// //Check if character is vowel or consonant /////////////////////////////////////////////////////
 
-const vowelCheck = (str) => {
-  const vowels = ["a", "e", "i", "o", "u"];
+// const vowelCheck = (str) => {
+//   const vowels = ["a", "e", "i", "o", "u"];
 
-  let vowel = 0;
-  let consonant = 0;
+//   let vowel = 0;
+//   let consonant = 0;
 
-  const strArr = str.toLowerCase().split("");
+//   const strArr = str.toLowerCase().split("");
 
-  for (let item of strArr) {
-    if (vowels.includes(item)) {
-      vowel++;
-    } else if (item >= "a" && item <= "z") {
-      consonant++;
-    }
-  }
-  return { vowel, consonant };
+//   for (let item of strArr) {
+//     if (vowels.includes(item)) {
+//       vowel++;
+//     } else if (item >= "a" && item <= "z") {
+//       consonant++;
+//     }
+//   }
+//   return { vowel, consonant };
+// };
+
+// // console.log(vowelCheck("Omit  lll77777 232232aooooollllll hassan RIpon"));
+
+// reverse string ////////////////////////////////
+
+const reverseStr = (params) => {
+  const strArr = [];
+  const str = params.split("");
+  str.forEach((element) => {
+    strArr.unshift(element);
+  });
+  const finalStr = strArr.join("");
+
+  return finalStr;
 };
 
-console.log(vowelCheck("Omit  lll77777 232232aooooollllll hassan RIpon"));
+console.log(reverseStr("mohebur"));
