982e9532034556943a4d906e0229579dc306a88b
SEMohebur <mdmohebur11@gmail.com>
2026-05-28 20:34:59 +0600

    find fizzBuzz

--- index.js
+++ index.js
@@ -180,3 +180,19 @@
 //   return arr;
 // };
 // findSingleValue(nums)
+
+const fizBuzz = (temp) => {
+  for (let i = 1; i <= temp; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(i, "FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log(i, "Fizz");
+    } else if (i % 5 === 0) {
+      console.log(i, "Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+fizBuzz(20);
