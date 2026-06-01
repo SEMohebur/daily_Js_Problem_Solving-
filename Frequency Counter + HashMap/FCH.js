2269e35031af4e8142961aa8444dda9549ace8e8
SEMohebur <mdmohebur11@gmail.com>
2026-06-01 18:04:29 +0600

    Count Character Frequency

--- Frequency Counter + HashMap/FCH.js
+++ Frequency Counter + HashMap/FCH.js
@@ -1,2 +1,18 @@
-// 1-7 soptaho ami Frequency Counter + HashMap pattern problem sikhbo
-console.log("FCH");
+// // 1-7 soptaho ami Frequency Counter + HashMap pattern problem sikhbo
+//  //console.log("FCH");
+
+const counterlatterFrequency = (param) => {
+  const result = {};
+  const strArr = Array.from(param);
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (!result[strArr[i]]) {
+      result[strArr[i]] = 1;
+    } else {
+      result[strArr[i]] = result[strArr[i]] + 1;
+    }
+  }
+  console.log(result);
+};
+
+counterlatterFrequency("javascriptssss");
