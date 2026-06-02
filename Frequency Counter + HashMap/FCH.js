99b5fbcce5e6c3c5a6de488291858814251266e8
SEMohebur <mdmohebur11@gmail.com>
2026-06-02 17:01:45 +0600

    find dublicate number

--- Frequency Counter + HashMap/FCH.js
+++ Frequency Counter + HashMap/FCH.js
@@ -13,23 +13,39 @@
 //       result[strArr[i]] = result[strArr[i]] + 1;
 //     }
 //   }
-//   console.log(result);
+//   return result
 // };
 
 // counterlatterFrequency("javascriptssss");
 
 // // number frequency counter 2===================================
 
-const numberFrequency = (param) => {
-  const result = {};
+// const numberFrequency = (param) => {
+//   const result = {};
+//   for (let i = 0; i < param.length; i++) {
+//     if (!result[param[i]]) {
+//       result[param[i]] = 1;
+//     } else {
+//       result[param[i]] = result[param[i]] + 1;
+//     }
+//   }
+//   return result;
+// };
+
+// console.log(numberFrequency([1, 2, 3, 6, 2, 5, 8, 9, 4, 2, 8]));
+
+// Find Duplicate Numbers 3====================================
+
+const FindDuplicateNumbers = (param) => {
+  const result = [];
   for (let i = 0; i < param.length; i++) {
-    if (!result[param[i]]) {
-      result[param[i]] = 1;
-    } else {
-      result[param[i]] = result[param[i]] + 1;
+    for (let j = i + 1; j < param.length; j++) {
+      if (param[i] == param[j]) {
+        result.push(param[j]);
+      }
     }
   }
-  console.log(result);
+  return result;
 };
 
-numberFrequency([1, 2, 3, 6, 2, 5, 8, 9, 4, 2, 8]);
+console.log(FindDuplicateNumbers([2, 5, 4, 2, 4, 9, 7, 6, 3, 5, 6, 8, 7, 0]));
