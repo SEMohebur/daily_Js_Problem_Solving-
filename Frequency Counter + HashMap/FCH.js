0ae3c1ac6aaf4199473bf7e66161706d276cd8bb
SEMohebur <mdmohebur11@gmail.com>
2026-06-02 17:22:14 +0600

    Tow Sum

--- Frequency Counter + HashMap/FCH.js
+++ Frequency Counter + HashMap/FCH.js
@@ -36,16 +36,31 @@
 
 // Find Duplicate Numbers 3====================================
 
-const FindDuplicateNumbers = (param) => {
-  const result = [];
-  for (let i = 0; i < param.length; i++) {
-    for (let j = i + 1; j < param.length; j++) {
-      if (param[i] == param[j]) {
-        result.push(param[j]);
+// const FindDuplicateNumbers = (param) => {
+//   const result = [];
+//   for (let i = 0; i < param.length; i++) {
+//     for (let j = i + 1; j < param.length; j++) {
+//       if (param[i] == param[j]) {
+//         result.push(param[j]);
+//       }
+//     }
+//   }
+//   return result;
+// };
+
+// console.log(FindDuplicateNumbers([2, 5, 4, 2, 4, 9, 7, 6, 3, 5, 6, 8, 7, 0]));
+
+// //Two Sum / use normalFunction for practice perpes 4======================================================
+
+function towSum(arr, targetNum) {
+  const resArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] == targetNum) {
+        resArr.push(arr.indexOf(arr[i]), arr.indexOf(arr[j]));
       }
     }
   }
-  return result;
-};
-
-console.log(FindDuplicateNumbers([2, 5, 4, 2, 4, 9, 7, 6, 3, 5, 6, 8, 7, 0]));
+  return resArr;
+}
+console.log(towSum([2, 7, 11, 15], 13));
