9aec0007c41d65449fa3e932e432b64abcb5e18d
SEMohebur <mdmohebur11@gmail.com>
2026-07-11 22:21:42 +0600

    palindrom

--- Two Pointer/TP.js
+++ Two Pointer/TP.js
@@ -1,19 +1,47 @@
 // //Two Pointer pattern sikhbo
 // //console.log("TP");
 
-const reverseString = (str) => {
-  let arr = str.split("");
+// //problem 1===========================================
+// //string reverse
+// const reverseString = (str) => {
+//   let arr = str.split("");
 
+//   let left = 0;
+//   let right = arr.length - 1;
+//   while (left < right) {
+//     let temp = arr[left];
+//     arr[left] = arr[right];
+//     arr[right] = temp;
+//     left++;
+//     right--;
+//   }
+//   return arr.join("");
+// };
+
+// console.log(reverseString("Hello"));
+
+// //problem 2 ====================================
+// // pelindrome
+
+const isPalindrome = (str) => {
+  const arr = str.split("");
   let left = 0;
   let right = arr.length - 1;
+
   while (left < right) {
-    let temp = arr[left];
-    arr[left] = arr[right];
-    arr[right] = temp;
+    let temp = arr[right];
+    arr[right] = arr[left];
+    arr[left] = temp;
+
     left++;
     right--;
   }
-  return arr.join("");
+  let res = arr.join("");
+  if (res === str) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-console.log(reverseString("Hello"));
+console.log(isPalindrome("101"));
