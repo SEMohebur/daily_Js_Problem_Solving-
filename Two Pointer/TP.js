d84294c2b002a9e464d4891cee3dcfddcbebb5f2
SEMohebur <mdmohebur11@gmail.com>
2026-07-14 23:34:09 +0600

    remove dublicate number

--- Two Pointer/TP.js
+++ Two Pointer/TP.js
@@ -125,21 +125,42 @@
 // problem 6 ============================================
 // Move Even Numbers Left
 
-const moveEvenNumLeft = (arr) => {
-  let left = 0;
-  let right = arr.length - 1;
+// const moveEvenNumLeft = (arr) => {
+//   let left = 0;
+//   let right = arr.length - 1;
 
-  while (left < right) {
-    if (arr[left] % 2 !== 0) {
-      let temp = arr[right];
-      arr[right] = arr[left];
-      arr[left] = temp;
+//   while (left < right) {
+//     if (arr[left] % 2 !== 0 || arr[right] % 2 == 0) {
+//       let temp = arr[right];
+//       arr[right] = arr[left];
+//       arr[left] = temp;
+//     }
+
+//     left++;
+//     right--;
+//   }
+//   return arr;
+// };
+
+// console.log(moveEvenNumLeft([2, 4, 1, 3, 6]));
+
+// problem 7 ============================================
+
+// // Remove Duplicates from Sorted Array
+
+const removeDuplicateNum = (arr) => {
+  if (arr.length === 0) return [];
+
+  let i = 0;
+
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
     }
-
-    left++;
-    right--;
   }
-  return arr;
+
+  return arr.slice(0, i + 1);
 };
 
-console.log(moveEvenNumLeft([1, 4, 3, 2, 7, 6]));
+console.log(removeDuplicateNum([1, 1, 2, 2, 3, 4, 4]));
